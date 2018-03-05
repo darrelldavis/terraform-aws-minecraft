c3aec170e23477062dca71274e5f1edcf972b9a0
Darrell Davis <dubldee@gmail.com>
2018-03-04 19:51:46 -0800

    added instance_id

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "public_ip" {
   value = "${module.ec2_minecraft.public_ip}"
 }
 
-output "instance_id" {
-  value = "${module.ec2_minecraft.ec2_instance_id}"
+output "id" {
+  value = "${module.ec2_minecraft.id}"
 }
